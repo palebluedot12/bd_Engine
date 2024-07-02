0585ecc13b0fa3a9af64ed3c3493ef669842d617
Sangmin Kim <sm524233@naver.com>
2024-07-03 00:23:58 +0900

    RenderManager / ResourceManager / SpriteRenderer 각각 역할을 잘 생각해보고 분리해보자... 할일 => ResourceManager 만들기

--- bdEngine/RenderManager.cpp
+++ bdEngine/RenderManager.cpp
@@ -1,4 +1,5 @@
 #include "RenderManager.h"
+#include <wincodec.h>
 #include <iostream>
 
 RenderManager::RenderManager()
@@ -28,6 +29,7 @@ void RenderManager::Render()
 {
 }
 
+
 void RenderManager::CreateDeviceResources()
 {
 	if (!pRenderTarget)
@@ -54,4 +56,63 @@ void RenderManager::DiscardDeviceResources()
 	if (pBrush) pBrush->Release();
 	pRenderTarget = nullptr;
 	pBrush = nullptr;
+}
+
+HRESULT RenderManager::CreateD2DBitmapFromFile(const WCHAR* szFilePath, ID2D1Bitmap** ppID2D1Bitmap)
+{
+	HRESULT hr;
+	// Create a decoder
+	IWICBitmapDecoder* pDecoder = NULL;
+	IWICFormatConverter* pConverter = NULL;
+
+	hr = pWICFactory->CreateDecoderFromFilename(
+		szFilePath,                      // Image to be decoded
+		NULL,                            // Do not prefer a particular vendor
+		GENERIC_READ,                    // Desired read access to the file
+		WICDecodeMetadataCacheOnDemand,  // Cache metadata when needed
+		&pDecoder                        // Pointer to the decoder
+	);
+
+	// Retrieve the first frame of the image from the decoder
+	IWICBitmapFrameDecode* pFrame = NULL;
+	if (SUCCEEDED(hr))
+	{
+		hr = pDecoder->GetFrame(0, &pFrame);
+	}
+
+	//Step 3: Format convert the frame to 32bppPBGRA
+	if (SUCCEEDED(hr))
+	{
+		hr = pWICFactory->CreateFormatConverter(&pConverter);
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		hr = pConverter->Initialize(
+			pFrame,                          // Input bitmap to convert
+			GUID_WICPixelFormat32bppPBGRA,   // Destination pixel format
+			WICBitmapDitherTypeNone,         // Specified dither pattern
+			NULL,                            // Specify a particular palette 
+			0.f,                             // Alpha threshold
+			WICBitmapPaletteTypeCustom       // Palette translation type
+		);
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		hr = GetRenderTarget()->CreateBitmapFromWicBitmap(pConverter, NULL, ppID2D1Bitmap);
+	}
+
+
+	// 파일을 사용할때마다 다시 만든다.
+	if (pConverter)
+		pConverter->Release();
+
+	if (pDecoder)
+		pDecoder->Release();
+
+	if (pFrame)
+		pFrame->Release();
+
+	return hr;
 }
--- bdEngine/RenderManager.h
+++ bdEngine/RenderManager.h
@@ -17,15 +17,19 @@ public:
 	~RenderManager();
 	void Initialize(HWND hwnd);
 	void Render();
+	HRESULT CreateD2DBitmapFromFile(const WCHAR* szFilePath, ID2D1Bitmap** ppID2D1Bitmap);
 	ID2D1HwndRenderTarget* GetRenderTarget() { return pRenderTarget; }
-
 
 private:
 	HWND m_Hwnd;
 	ID2D1Factory* pFactory;
 	ID2D1HwndRenderTarget* pRenderTarget;
 	ID2D1SolidColorBrush* pBrush;
+	
+	// D2D1비트맵 생성을 위한 이미지 처리 인터페이스
+	IWICImagingFactory* pWICFactory;
 
+	
 	GameObject m_Player; // 이거 Scene에서 들고있을거임
 
 	void CreateDeviceResources();
